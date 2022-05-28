e34b78ca591c43d859b13536c405e62245e96d10
Roggerton <84243271+Roggerton@users.noreply.github.com>
2022-05-28 08:51:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    setTimeout(function(){
+        img_id="selfie1";
+        take_snapshot();
+        speak_data="This is taking your next selfie in just 10 seconds. Except for bad internet connection, which will stop this and you would have to do it again. But for the time being, s m i l e !";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+    }, 5000);
+
+    setTimeout(function(){
+        img_id="selfie2";
+        take_snapshot();
+        speak_data="This is taking your next selfie in just 15 seconds. Except for bad internet connection, which will stop this and you would have to do it again. But for the time being, s m i l e !";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+    }, 10000);
+
+setTimeout(function(){
+    img_id="selfie3";
+    take_snapshot();
+}, 15000);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+function take_snapshot(){
+    console.log(img_id);
+    Webcam.snap(function(data_uri){
+        if(img_id == "selfie1"){
+            document.getElementById("result1").innerHTML='<img id="selfie1" src="'+data_url+'">';
+        }
+        if(img_id == "selfie2"){
+            document.getElementById("result2").innerHTML='<img id="selfie2" src="'+data_url+'">';
+        }
+        if(img_id == "selfie3"){
+            document.getElementById("result3").innerHTML='<img id="selfie3" src="'+data_url+'">';
+        }
+    });
+}
+
+
